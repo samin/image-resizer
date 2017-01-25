f81931566012b4bf1f88f2d3fb4a7b62db33f90e
Samin Shams <saminshams@gmail.com>
2017-01-25 11:06:37 -0200

    sharp method updated

--- src/streams/resize.js
+++ src/streams/resize.js
@@ -90,10 +90,7 @@ module.exports = function () {
             d.resize.width,
             d.resize.height
           ).extract(
-            d.crop.y,
-            d.crop.x,
-            d.crop.width,
-            d.crop.height
+            { left: d.crop.x, top: d.crop.y, width: d.crop.width, height: d.crop.height }
           );
 
         r.toBuffer(resizeResponse);
